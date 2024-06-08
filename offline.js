2fbc549c595d283524db34fbf1b0ef725fb88a64
MKettermann <ultramk10@gmail.com>
2024-06-08 19:58:40 -0300

    offline padrao

--- offline.js
+++ offline.js
@@ -5,64 +5,85 @@
 if ("serviceWorker" in navigator) {
 	// Registro do SW
 	navigator.serviceWorker.register("./sw.js").then((reg) => {
-		mkt.l(`%cSW Registrado em ${reg.scope}`, "color:green;");
+		console.log(`%c>> SW_OUT:%c Registro DONE -> ${reg.scope}`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 	}).catch((err) => {
-		mkt.l("%cRegistrar SW Falhou: ", "color:red;background-color:black;", err);
+		console.log(`%c>> SW_OUT:%c Registro FAIL ->`, "color:MediumSpringGreen;", "color:MediumOrchid;", err);
 	});
 
 	// Verificação da Versão Atual
+	// Primeira vez que entra: acredito que gere um erro por causa do assincrono do .registrar() ainda não concluiu, e aqui já ta dando .getRegistration().
 	navigator.serviceWorker.getRegistration()
 		.then(reg => {
 			reg.addEventListener("updatefound", (ev) => {
 				const swInstall = reg.installing;
 				swInstall.addEventListener("statechange", () => {
-					if (swInstall.state == "installed") {
+					if (swInstall.state == "installed") { // installed / activating / activated
 						// Nova Versão Instalada (Informar usuário)
-						mkt.l("%cNova Versão Instalada e Waiting Reload. ", "color:lime;");
-						if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = "Uma nova versão foi encontrada. Atualize.";
+						console.log(`%c>> SW_OUT:%c Nova versão instalada.`, "color:MediumSpringGreen;", "color:MediumOrchid;");
+
+						// ---
+						if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = "Nova versão encontrada. Atualize.";
 						if (mkt.Q("#indexRefresh")) mkt.QverOn("#indexRefresh");
+						// ---
+
 					} else {
-						// Atual em uso
-						mkt.l("%cNova versão atualmente em uso.", "color:green;");
-						if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = "Nova Versão instalada.";
+						console.log(`%c>> SW_OUT:%c Estado da versão: ${swInstall.state}`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 					}
 				})
 			})
 		}).catch(err => {
-			mkt.l("%cErro durante verificação da versão atual.", "color:red;background-color:black;");
+			console.log(`%c>> SW_OUT:%c Falhou em obter a versão atual:`, "color:MediumSpringGreen;", "color:MediumOrchid;", err);
 		});
 
-	//
+	// COMUNICAÇÃO
 	navigator.serviceWorker.addEventListener("message", ev => {
 		switch (ev.data.action) {
 			case "cache-atualizado":
-				if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = "Cache Atualizado.";
+				console.log(`%c>> SW_OUT:%c Dados Atualizados. Versão: ${ev.data.ver}`, "color:MediumSpringGreen;", "color:MediumOrchid;");
+
+				// ---
+				if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = "Cache Atualizado. Ver: " + ev.data.ver;
+				// ---
+
 				break;
 			case "sync":
-				mkt.l("%cSync recebido:", "color:green;", ev.data.str);
-				if (mkt.Q("#swOutputInfo")) mkt.Q("#swOutputInfo").innerHTML = `Sync Tag: ${ev.data.str}`;
+				console.log(`%c>> SW_OUT:%c INFO SINCRONIZADO. Versão: ${ev.data.ver}`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 				break;
 		}
 	})
-
 } else {
-	mkt.l("%cSem suporte a Service Worker (Verificar HTTPS)", "color:red;background-color:black;");
+	console.log(`%c>> SW_OUT:%c Sem suporte a Service Worker (Verificar HTTPS).`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 }
 
 class mkSw {
 	// Atualizar no SW.
 	static getUpdate = () => {
-		mkt.l("%c>> Update Register.", "color:gold;");
+		console.log(`%c>> SW_OUT:%c >> Solicitando Update...`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 		navigator.serviceWorker?.getRegistration().then(reg => {
 			reg.update();
 		});
 	}
 	// Desregistrar no SW.
 	static del = () => {
-		mkt.l("%c>> Desregistrando SW.", "color:gold;");
+		console.log(`%c>> SW_OUT:%c >> Desregistrando Serviço...`, "color:MediumSpringGreen;", "color:MediumOrchid;");
 		navigator.serviceWorker?.getRegistration().then(reg => {
 			reg.unregister();
 		});
+	}
+	// Message To SW. Via Controller
+	static sendMessageToSW = (message) => {
+		console.log(`%c>> SW_OUT:%c >> Comunicação:`, "color:MediumSpringGreen;", "color:MediumOrchid;", message);
+		if (navigator.serviceWorker.controller) {
+			navigator.serviceWorker.controller.postMessage(message)
+		} else {
+			console.log(`%c>> SW_OUT:%c >> Sem controller para enviar mensagem: `, "color:MediumSpringGreen;", "color:MediumOrchid;", message);
+		}
+	}
+	// Update All Clients via Message
+	static requestCacheUpdate = () => {
+		mkSw.sendMessageToSW({
+			action: "UpdateCache"
+		})
 	}
 	// Registrar pra Sincronizar
 	// static returnTagOnSync = (tag) => {
@@ -73,19 +94,4 @@ class mkSw {
 	// 		});
 	// 	}
 	// }
-	// Message To SW. Via Controller
-	static sendMessageToSW = (message) => {
-		mkt.l("%c>> To SW:", "color:gold;", message);
-		if (navigator.serviceWorker.controller) {
-			navigator.serviceWorker.controller.postMessage(message)
-		} else {
-			mkt.l("%cSem controller para enviar mensagem.", "color:red;");
-		}
-	}
-	// Update All Clients via Message
-	static requestCacheUpdate = () => {
-		mkSw.sendMessageToSW({
-			action: "cacheUpdate"
-		})
-	}
 }
