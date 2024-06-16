f198c1d9a25c8b0477bd29ee4dd6ebba8afe67f0
MKettermann <ultramk10@gmail.com>
2024-06-15 23:41:21 -0300

    fix sw

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  MK Service Worker               \\
 //__________________________________*/
 
-const version = "1.73";
+const version = "1.74";
 let cacheon = new URL(location.href).searchParams.get("cache");
 if (cacheon == "true") { cacheon = true } else { cacheon = false };
 let log = new URL(location.href).searchParams.get("log");
