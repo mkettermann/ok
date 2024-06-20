4e244d8ce36b2a6f5f9428a31d78abfa56670807
MKettermann <ultramk10@gmail.com>
2024-06-20 17:27:56 -0300

    up sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker Versão
-const version = "1.91";
+const version = "1.104";
 
 // Assets que serão salvos quando instalar o sw.
 const swAssets = [
@@ -39,18 +39,10 @@ let log_level = new URL(location.href).searchParams.get("log");
 if (log_level == null) { log_level = 0; }
 let f = new URL(location.href).searchParams.get("f"); // ativa / desativa found (encontrado fora dos assets)
 if (f != "false") { f = true; } else { f = false; };
-let p = new URL(location.href).searchParams.get("p");
-if (p == null) { p = 1; } else { p = Number(p); } // Padrão CacheFirst
+let politica = new URL(location.href).searchParams.get("p");
+if (politica == null) { politica = 1; } else { politica = Number(politica); } // Padrão CacheFirst
 const cacheName = "sw_v_static_" + version; // Assets ao instalar
 const cacheFound = "sw_v_found_" + version; // Assets que irá guardar durante.
-
-
-
-const filesToCache = [
-	...swAssets,
-];
-// const debug = false;
-// const log = debug ? console.log.bind(console) : () => {};
 self.numBadges = 0;
 
 // Inside of Worker
@@ -95,36 +87,54 @@ const sw_cacheSave = (url, inNetwork) => {
 
 };
 
-const sw_messageToClients = (action, str = "") => {
+const getClients = async () => await self.clients.matchAll({ includeUncontrolled: true });
+
+const hasActiveClients = async () => {
+	const clients = await getClients();
+	return clients.some(({ visibilityState }) => visibilityState === 'visible');
+};
+
+const sw_messageToClients = async (msg, str = "") => {
+	const clients = await getClients();
 	let obj = {
-		type: action,
+		type: msg,
 		str: str,
 		ver: version
 	}
 	showInfo(">> COMUNICAÇÃO", obj);
-	clients.matchAll({
-		includeUncontrolled: false,
-		type: "window",
-	}).then(clients => {
-		clients.forEach(client => { client.postMessage(obj) });
-	})
+	clients.forEach((client) => { client.postMessage(obj) });
 }
+
+
 
 // Comunicação
 self.addEventListener('message', async (ev) => {
-	let obj = ev.data;
-	showInfo("<< COMUNICAÇÃO", obj);
-	switch (obj.type) {
+	const { type } = ev.data;
+	showInfo("<< COMUNICAÇÃO", ev.data);
+	switch (type) {
+
 		case "PREPARE_CACHES_FOR_UPDATE":
 			ev.waitUntil(sw_cacheUpdate(cacheName).then(r => {
-				sw_messageToClients("UpdateFull");
+				sw_messageToClients("UPDATED_CACHE");
 			}));
 			break;
+
 		case "SKIP_WAITING":
-			self.skipWaiting();
+			const clients = await getClients();
+			if (clients.length < 2) {
+				self.skipWaiting();
+			}
 			break;
-		case "Versão":
-			sw_messageToClients("Versão");
+
+		case 'CLEAR_BADGES':
+			self.numBadges = 0;
+			if ('clearAppBadge' in navigator) {
+				navigator.clearAppBadge();
+			}
+			break;
+
+		case "VERSION":
+			sw_messageToClients("VERSION");
 			break;
 	}
 });
@@ -137,8 +147,8 @@ self.addEventListener('install', ev => {
 
 // Ao ativar nova versão
 self.addEventListener("activate", ev => {
-	showInfo(`Anunciando NOVA Versao ATIVA (${version}). Removendo Antigas...`, "ATIVO");
-	sw_messageToClients("Versão");
+	showInfo(`NOVA Versao ATIVA (${version}). Removendo Antigas...`, "ATIVO");
+	sw_messageToClients("VERSION");
 	// Aqui limpa versoes antigas.
 	// Possivel problema: 2 Abas, usando o mesmo cache,
 	//   mas uma aba avança versão e a outra não.
@@ -156,12 +166,13 @@ self.addEventListener("activate", ev => {
 
 // // POLÍTICAS SW PROXY
 self.addEventListener("fetch", ev => {
+	// Verificar possibilidade de diferentes políticas pra diferentes tipos de request.
 	let url = new URL(ev.request.url);
 	let checkUrl = url.origin + url.pathname;
 	if (url.origin == location.origin) {
 		checkUrl = url.pathname; // Apenas Path dos assets
 	}
-	switch (p) {
+	switch (politica) {
 		case 0:
 			// 0 - Stale-While-Revalidate
 			ev.respondWith(
@@ -224,7 +235,7 @@ self.addEventListener("fetch", ev => {
 })
 
 
-// -----
+// OUTRAS MECANICAS
 
 const IDBConfig = {
 	name: 'pwa-db',
@@ -235,227 +246,41 @@ const IDBConfig = {
 	}
 };
 
-// const templateFolder = '/src/templates';
-// const header = `${templateFolder}/header.html`;
-// const footer = `${templateFolder}/footer.html`;
-
-const createIndexedDB = ({ name, store }) => {
-	const request = self.indexedDB.open(name, 1);
-	return new Promise((resolve, reject) => {
-		request.onupgradeneeded = e => {
-			const db = e.target.result;
-			if (!db.objectStoreNames.contains(store.name)) {
-				db.createObjectStore(store.name, { keyPath: store.keyPath });
-				showInfo('IDB Store Criado', store.name);
-			}
-			[...db.objectStoreNames].filter((name) => name !== store.name).forEach((name) => db.deleteObjectStore(name));
-		};
-		request.onsuccess = () => resolve(request.result);
-		request.onerror = () => reject(request.error);
-	});
-};
-
 const getStoreFactory = (dbName) => ({ name }, mode = 'readonly') => {
 	return new Promise((resolve, reject) => {
-
 		const request = self.indexedDB.open(dbName, 1);
-
 		request.onsuccess = e => {
 			const db = request.result;
 			const transaction = db.transaction(name, mode);
 			const store = transaction.objectStore(name);
-
 			return resolve(store);
 		};
-
 		request.onerror = e => reject(request.error);
 	});
 };
-
 const openStore = getStoreFactory(IDBConfig.name);
-
 const getCacheStorageNames = async () => {
 	const cacheNames = await caches.keys() || [];
 	const outdatedCacheNames = cacheNames.filter(name => !name.includes(cacheName));
 	const latestCacheName = cacheNames.find(name => name.includes(cacheName));
-
 	return { latestCacheName, outdatedCacheNames };
 };
 
-const prepareCachesForUpdate = async () => {
-	const { latestCacheName, outdatedCacheNames } = await getCacheStorageNames();
-	if (!latestCacheName || !outdatedCacheNames?.length) {
-		return null;
-	}
-	const latestCache = await caches?.open(latestCacheName);
-	const latestCacheKeys = (await latestCache?.keys())?.map(c => c.url) || [];
-	const latestCacheMainKey = latestCacheKeys?.find(url => url.includes('/index.html'));
-	const latestCacheMainKeyResponse = latestCacheMainKey ? await latestCache.match(latestCacheMainKey) : null;
-	const latestCacheOtherKeys = latestCacheKeys.filter(url => url !== latestCacheMainKey) || [];
-	const cachePromises = outdatedCacheNames.map(cacheName => {
-		const getCacheDone = async () => {
-			const cache = await caches?.open(cacheName);
-			const cacheKeys = (await cache?.keys())?.map(c => c.url) || [];
-			const cacheMainKey = cacheKeys?.find(url => url.includes('/index.html'));
-			if (cacheMainKey && latestCacheMainKeyResponse) {
-				await cache.put(cacheMainKey, latestCacheMainKeyResponse.clone());
-			}
-			return Promise.all(
-				latestCacheOtherKeys
-					.filter(key => !cacheKeys.includes(key))
-					.map(url => cache.add(url).catch(r => console.error(r))),
-			);
-		};
-		return getCacheDone();
-	});
-	return Promise.all(cachePromises);
-};
+// NOTIFICATION
+// Close
+self.addEventListener('notificationclick', async (ev) => {
+	ev.notification.close();
+})
 
-// const getStreamedHtmlResponse = (url) => {
-// 	const stream = new ReadableStream({
-// 		async start(controller) {
-// 			const pushToStream = stream => {
-// 				const reader = stream.getReader();
-// 				return reader.read().then(function process({ value, done }) {
-// 					if (done) {
-// 						return;
-// 					}
-// 					controller.enqueue(value);
-// 					return reader.read().then(process);
-// 				});
-// 			};
-
-// 			const templates = [
-// 				caches.match(header),
-// 				caches.match(`${templateFolder}${url}.html`),
-// 				caches.match(footer),
-// 			];
-// 			const responses = await Promise.all(templates);
-
-// 			for (const template of responses) {
-// 				await pushToStream(template.body);
-// 			}
-
-// 			controller.close();
-// 		}
-// 	});
-
-
-// 	return new Response(stream, {
-// 		headers: { 'Content-Type': 'text/html; charset=utf-8' }
-// 	});
-// };
-
-const installHandler = e => {
-	e.waitUntil(
-		self.clients.matchAll({
-			includeUncontrolled: true,
-		})
-			.then(clients => {
-				caches.open(cacheName)
-					.then(cache => cache.addAll(filesToCache.map(file => new Request(file, { cache: 'no-cache' }))))
-			})
-			.catch(err => console.error('cache error', err))
-	);
-};
-
-const activateHandler = e => {
-	e.waitUntil(
-		caches.keys()
-			.then(names => Promise.all(
-				names
-					.filter(name => name !== cacheName)
-					.map(name => caches.delete(name))
-			))
-	);
-};
-
-// const _fetchHandler = async e => {
-// 	const { request } = e;
-// 	const { url, method, headers, mode, credentials, cache } = request;
-// 	// if (url.includes('google')) {
-// 	// 	return false;
-// 	// }
-// 	showInfo('Fetch', [url, request.method]);
-// 	e.respondWith(
-// 		caches.match(request, { ignoreVary: true, ignoreSearch: true })
-// 			.then(inCache => {
-// 				if (inCache) {
-// 					return inCache;
-// 				}
-// 				if (url.startsWith(location.origin) && !url.match(/\.[a-zA-Z]{2,4}$/)) {
-// 					const indexUrl = url.endsWith('/') ? `${url}index.html` : `${url}/index.html`;
-// 					showInfo('Tentando coletar Index', indexUrl);
-// 					const indexRequest = new Request(indexUrl, { method, headers, credentials, cache });
-// 					return caches.match(indexRequest, { ignoreSearch: true })
-// 				}
-// 				return fetch(e.request);
-// 			})
-// 			.then(inNetwork => {
-// 				if (inNetwork) {
-// 					return inNetwork;
-// 				}
-// 				showInfo('Sem resposta da URL', url);
-// 				return fetch(e.request);
-// 			})
-// 			.catch(err => console.error('fetch error:', 'url:', url, 'error:', err))
-// 	);
-// };
-
-// const fetchHandler = async e => {
-// 	const { request } = e;
-// 	const { url, method, headers, mode, credentials, cache } = request;
-// 	// if (url.includes('google')) {
-// 	// 	return false;
-// 	// }
-// 	showInfo('Fetch', [url, request.method]);
-// 	const { origin, pathname } = new URL(url);
-// 	if (origin === location.origin && !pathname.match(/\.[a-zA-Z0-9-]{2,5}$/)) {
-// 		const pageURL = pathname === '/' ? '/home' : pathname;
-// 		e.respondWith(getStreamedHtmlResponse(pageURL));
-// 	} else {
-// 		e.respondWith(
-// 			caches.match(request, { ignoreVary: true, ignoreSearch: true })
-// 				.then(inCache => {
-// 					if (inCache) {
-// 						return inCache;
-// 					}
-// 					return fetch(e.request);
-// 				})
-// 				.then(inNetwork => {
-// 					if (inNetwork) {
-// 						return inNetwork;
-// 					}
-// 					showInfo('Sem resposta da URL', url);
-// 					return fetch(e.request);
-// 				})
-// 				.catch(err => showError('Fail to Fetch', err))
-// 		);
-// 	}
-// };
-
-const getClients = async () => await self.clients.matchAll({
-	includeUncontrolled: true,
-});
-
-const hasActiveClients = async () => {
-	const clients = await getClients();
-	return clients.some(({ visibilityState }) => visibilityState === 'visible');
-};
-
-const sendMessage = async message => {
-	const clients = await getClients();
-	clients.forEach((client) => client.postMessage({ type: 'message', message }));
-}
-
-const pushHandler = async e => {
-	const data = e.data.json();
+// Push
+self.addEventListener('push', async (ev) => {
+	const data = ev.data.json();
 	const { title, message, interaction } = data;
 
 	const options = {
 		body: message,
 		icon: '/src/img/icons/icon-512x512.png',
-		vibrate: [100, 50, 100],
+		vibrate: [100, 30, 100, 30, 100],
 		data: {
 			dateOfArrival: Date.now()
 		},
@@ -472,7 +297,7 @@ const pushHandler = async e => {
 		requireInteraction: interaction
 	};
 
-	e.waitUntil(
+	ev.waitUntil(
 		self.registration.showNotification(title, options)
 			.then(hasActiveClients)
 			.then((activeClients) => {
@@ -481,54 +306,19 @@ const pushHandler = async e => {
 					navigator.setAppBadge(self.numBadges);
 				}
 			})
-			.catch(err => sendMessage(err))
+			.catch(err => sw_messageToClients(err))
 	)
-};
+});
 
-const messageHandler = async ({ data }) => {
-	const { type } = data;
-	switch (type) {
-
-		// case 'transition-type-change':
-		// 	const { transitionType } = data;
-		// 	const headerResponse = await (await caches.match(header)).text();
-		// 	const newHeader = headerResponse.replace(/data-transition="(.+?)"/,
-		// 		`data-transition="${transitionType}"`);
-		// 	const headers = { headers: { 'Content-Type': 'text/html; charset=utf-8' } };
-		// 	const cache = await caches.open(cacheName);
-		// 	await cache.put(header, new Response(newHeader, headers));
-		// 	break;
-
-		case 'clearBadges':
-			self.numBadges = 0;
-			if ('clearAppBadge' in navigator) {
-				navigator.clearAppBadge();
-			}
-			break;
-
-		case 'SKIP_WAITING':
-			const clients = await self.clients.matchAll({
-				includeUncontrolled: true,
-			});
-			if (clients.length < 2) {
-				self.skipWaiting();
-			}
-			break;
-
-		case 'PREPARE_CACHES_FOR_UPDATE':
-			await prepareCachesForUpdate();
-			break;
-	}
-}
-
-const syncHandler = async e => {
+// Online
+self.addEventListener('sync', async ev => {
 	const title = 'Background Sync demo';
 	const message = 'Background Sync demo message';
-	if (e.tag.startsWith('sync-demo')) {
+	if (ev.tag.startsWith('sync-demo')) {
 		const options = {
 			body: message,
 			icon: '/src/img/icons/icon-512x512.png',
-			vibrate: [100, 50, 100],
+			vibrate: [100, 30, 100, 30, 100],
 			data: {
 				dateOfArrival: Date.now()
 			},
@@ -539,7 +329,7 @@ const syncHandler = async e => {
 				},
 				{
 					action: 'close',
-					title: 'Close notification'
+					title: 'Fechar'
 				},
 			]
 		};
@@ -556,7 +346,7 @@ const syncHandler = async e => {
 					request.onerror = e => reject(e);
 				})
 		});
-		e.waitUntil(
+		ev.waitUntil(
 			getNotifications()
 				.then((notifications) => {
 					console.log(notifications);
@@ -571,23 +361,10 @@ const syncHandler = async e => {
 				})
 		)
 	}
-};
+});
 
-const notificationClickHandler = async e => {
-	e.notification.close();
-};
-
-// self.addEventListener('install', installHandler);
-// self.addEventListener('activate', activateHandler);
-// self.addEventListener('fetch', fetchHandler);
-self.addEventListener('push', pushHandler);
-self.addEventListener('notificationclick', notificationClickHandler)
-self.addEventListener('sync', syncHandler);
-// self.addEventListener('message', messageHandler);
-
+// Fetch in background
 self.addEventListener('backgroundfetchsuccess', async (e) => {
 	const { id } = e.registration;
-	const clients = await getClients();
-
-	clients.forEach((client) => client.postMessage({ type: 'background-fetch-success', id }));
+	sw_messageToClients("background-fetch-success", id)
 });
