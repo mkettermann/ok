6e0029d08a7eca4b6c9ba0d94ac09cc5cfef3a63
MKettermann <ultramk10@gmail.com>
2024-06-19 22:36:42 -0300

    tools

--- sw_tools.js
+++ sw_tools.js
@@ -1,134 +1,160 @@
-"use strict"
+"use strict";
 /**********************************\\
 //  MK SW - Tools                   \\
 //__________________________________*/
 // Versionamento, Cacheamento, PWA
 
-class mkSw {
+class mksw {
 	static config = null;
 	static workerUrl = "";
 	static updateConcluido = null;
 
+	static installButton = null;
+	static installContainer = null;
+
 	// Registra e vincula a função de cada evento
 	static start = async (config) => {
-		mkSw.config = config;
-		if (typeof mkSw.config == "object") {
-			if (mkSw.config.p == null) {
-				mkSw.config.p = 1;
+		mksw.config = config;
+		if (typeof mksw.config == "object") {
+			if (mksw.config.p == null) {
+				mksw.config.p = 1;
 			}
-			if (mkSw.config.f == null) {
-				mkSw.config.f = "";
+			if (mksw.config.f == null) {
+				mksw.config.f = "";
 			}
-			if (mkSw.config.log != null) {
-				mkSw.config.log = mkSw.config.log;
+			if (mksw.config.log != null) {
+				mksw.config.log = mksw.config.log;
 			} else {
-				mkSw.config.log = 1;
+				mksw.config.log = 1;
 			}
-			if (mkSw.config.url == null) {
+			if (mksw.config.url == null) {
 				let url_path = location.pathname;
 				let liof = url_path.lastIndexOf("/");
 				if (liof >= 0) {
 					url_path = location.pathname.slice(0, liof) + "/";
 				}
 				let fullUrl = location.origin + url_path;
-				mkSw.showInfo("Full Url", fullUrl, 2)
-				mkSw.workerUrl = new URL(fullUrl);
-				mkSw.workerUrl.pathname += "sw.js";
+				mksw.showInfo("Full Url", fullUrl, 2)
+				mksw.workerUrl = new URL(fullUrl);
+				mksw.workerUrl.pathname += "sw.js";
 			} else {
-				mkSw.workerUrl = new URL(mkSw.config.url);
+				mksw.workerUrl = new URL(mksw.config.url);
 			}
-			if (mkSw.config.aoAtualizarVersao != null) {
-				mkSw.aoAtualizarVersao = mkSw.config.aoAtualizarVersao;
+			if (mksw.config.aoAtualizarVersao != null) {
+				mksw.aoAtualizarVersao = mksw.config.aoAtualizarVersao;
 			}
-			mkSw.workerUrl.searchParams.set("p", mkSw.config.p);
-			mkSw.workerUrl.searchParams.set("f", mkSw.config.f);
-			mkSw.workerUrl.searchParams.set("log", mkSw.config.log);
-			mkSw.config.url = mkSw.workerUrl.href;
-			mkSw.config.versao = mkSw._getVersaoAtual();
+			mksw.workerUrl.searchParams.set("p", mksw.config.p);
+			mksw.workerUrl.searchParams.set("f", mksw.config.f);
+			mksw.workerUrl.searchParams.set("log", mksw.config.log);
+			mksw.config.url = mksw.workerUrl.href;
+			mksw.config.versao = mksw._getVersaoAtual();
 		}
 		// Não iniciar quando indisponível
 		if (!("serviceWorker" in navigator)) {
-			mkSw.showError("Sem suporte a Service Worker (Verificar HTTPS)", "")
+			mksw.showError("Sem suporte a Service Worker (Verificar HTTPS)", "")
 			return null;
 		}
 
 		// Registrar do SW
-		let registro = await navigator.serviceWorker.register(mkSw.workerUrl.href).catch((err) => mkSw.showError("Register", err));
+		await navigator.serviceWorker.register(mksw.workerUrl.href).catch((err) => mksw.showError("Register", err));
+		const registro = await navigator.serviceWorker.ready;
 
-		// Valida Registro
-		// let registro = await navigator.serviceWorker.getRegistration().catch((err) => mkSw.showError("GET Registro", err));
-		if (registro == null) {
-			mkSw.showError("Falhou em obter a versão atual", registro);
-			return null;
+		// Finaliza Start se não houver Registro
+		// if (registro == null) {
+		// 	mksw.showError("Falhou em obter a versão atual", registro);
+		// 	return null;
+		// }
+
+		if (registro.waiting && registro.active) {
+			mksw.showInfo("Opa! Nova versão encontrada", registro.waiting, registro.active);
+			window.swNeedUpdate = true;
 		}
 
 		// GATILHO de UPDATE (Só se executa se o SW for modificado)
-		navigator.serviceWorker.getRegistration().then(reg => {
-			mkSw.showInfo("Registro bem sucedido", registro.scope, 1)
-			mkSw.showInfo("Log Level", mkSw.config.log);
-			mkSw.showInfo("Política", mkSw.config.p);
-			mkSw.showInfo("Cache new itens", mkSw.config.f);
-
-			if (reg) reg.onupdatefound = (ev) => {
-				const instalacao = reg.installing;
-
-				if (instalacao) instalacao.onstatechange = () => {
-					mkSw.showInfo("Estado da instalação: ", instalacao.state);
-					if (instalacao.state == "installed") { // installed / activating / activated
-						mkSw.skipWaiting();
-					}
-				};
-
+		registro.onupdatefound = (ev) => {
+			const swInstalando = registro.installing;
+			if (swInstalando) swInstalando.onstatechange = async () => {
+				if (swInstalando.state === 'installed' && navigator.serviceWorker.controller) {
+					mksw.showInfo("SW sob controle:", swInstalando.state);
+					window.swNeedUpdate = true;
+					await mksw.prepareCachesForUpdate();
+					await mksw.skipWaiting();
+				}
 			};
+		}
 
-		}).catch(err => { mkSw.showError("Falhou", err) })
+		// GATILHO de SAIDA
+		window.addEventListener('beforeunload', async () => {
+			if (window.swNeedUpdate) {
+				mksw.showInfo("Antes de Sair. Skip Updates");
+				window.swNeedUpdate = false;
+				await mksw.skipWaiting();
+			}
+		});
 
 		// GATILHO de COMUNICAÇÃO
 		navigator.serviceWorker.addEventListener("message", ev => {
-			mkSw.showInfo("<< COMUNICAÇÃO", ev.data);
-			switch (ev.data.action) {
+			mksw.showInfo("<< COMUNICAÇÃO", ev.data);
+			switch (ev.data.type) {
 				case "UpdateFull":
-					mkSw.showInfo("Update Concluído", ev.data.ver);
-					mkSw.updateConcluido = ev.data.ver;
+					mksw.showInfo("Update Concluído", ev.data.ver);
+					mksw.updateConcluido = ev.data.ver;
 					break;
 				case "Versão":
-					if (ev.data.ver != mkSw._getVersaoAtual()) {
-						let listaVersoes = [mkSw._getVersaoAtual(), ev.data.ver];
-						mkSw.showInfo(`Versão MUDOU:`, listaVersoes);
-						mkSw._setVersaoAtual(ev.data.ver);
-						mkSw.aoAtualizarVersao(listaVersoes[1], listaVersoes[0]);
+					if (ev.data.ver != mksw._getVersaoAtual()) {
+						let listaVersoes = [mksw._getVersaoAtual(), ev.data.ver];
+						mksw.showInfo(`Versão MUDOU:`, listaVersoes);
+						mksw._setVersaoAtual(ev.data.ver);
+						mksw.aoAtualizarVersao(listaVersoes[1], listaVersoes[0]);
 					}
 					break;
 				case "sync":
-					mkSw.showInfo("Sincronizado", ev.data.ver);
+					mksw.showInfo("Sincronizado", ev.data.ver);
 					break;
 			}
 		})
 
-		return mkSw.config;
+		return mksw.config;
 	}
 
-	// Out of Worker
+	// SW Erros
 	static showError = (msg, erro) => {
 		console.error(`%cO> %cSW_ERRO: %c${msg}%c ->`, "color:MediumOrchid;", "color:MediumSpringGreen;", "background:#0009;color:red;border-radius:3px;padding:0px 3px;", "color:MediumOrchid;", erro);
 	}
 
-	// Out of Worker
+	// SW Info
 	static showInfo = (msg, data, nivel = 2) => {
-		if (mkSw.config.log >= nivel) console.log(`%cO> %cSW_INFO: %c${msg}%c ->`, "color:MediumOrchid;", "color:MediumSpringGreen;", "background:#0005;color:green;border-radius:3px;padding:0px 3px;", "color:MediumOrchid;", data);
+		if (mksw.config.log >= nivel) console.log(`%cO> %cSW_INFO: %c${msg}%c ->`, "color:MediumOrchid;", "color:MediumSpringGreen;", "background:#0005;color:green;border-radius:3px;padding:0px 3px;", "color:MediumOrchid;", data);
+	}
+
+	// Coleta dos Workers em estado de Aguardo
+	static getWaitingWorker = async () => {
+		const registrations = await navigator?.serviceWorker?.getRegistrations() || [];
+		const registrationWithWaiting = registrations.find(r => r.waiting);
+		return registrationWithWaiting?.waiting;
+	}
+
+	// Skip Waiting
+	static skipWaiting = async () => {
+		return (await mksw.getWaitingWorker())?.postMessage({ type: 'SKIP_WAITING' });
+	}
+
+	// Solicita Refazer Cache.
+	static prepareCachesForUpdate = async () => {
+		return (await mksw.getWaitingWorker())?.postMessage({ type: 'PREPARE_CACHES_FOR_UPDATE' });
 	}
 
 	static aoAtualizarVersao = (versao) => {
-		mkSw.showInfo("fn aoAtualizarVersao recebeu: ", versao);
+		mksw.showInfo("fn aoAtualizarVersao recebeu: ", versao);
 	}
 
 	// Atualizar no SW.
 	static getUpdate = () => {
 		return new Promise((r, re) => {
-			mkSw.showInfo("Solicitando Update de Versão...");
+			mksw.showInfo("Solicitando Update de Versão...");
 			navigator.serviceWorker?.getRegistration().then(reg => {
 				reg.update();
-				mkSw.skipWaiting();
+				mksw.skipWaiting();
 				r();
 			}).catch(erro => {
 				re(erro);
@@ -137,43 +163,43 @@ class mkSw {
 	}
 	// Solicita Versao.
 	static getVersao = () => {
-		if (!mkSw.sendMessageToSW({ action: "Versão" })) {
+		if (!mksw.sendMessageToSW({ action: "Versão" })) {
 			re("Falha de comunicação");
 		}
 	}
 	// Desregistrar no SW.
 	static del = () => {
-		mkSw.showInfo("Desregistrando Serviço...");
+		mksw.showInfo("Desregistrando Serviço...");
 		navigator.serviceWorker?.getRegistration().then(reg => {
 			reg.unregister().then(r => {
-				mkSw.showInfo("SW Desregistrado.");
+				mksw.showInfo("SW Desregistrado.");
 			});
 		});
 	}
 	// Message To SW. Via Controller
 	static sendMessageToSW = (message) => {
-		mkSw.showInfo(">> COMUNICAÇÃO", message);
+		mksw.showInfo(">> COMUNICAÇÃO", message);
 		if (navigator.serviceWorker.controller) {
 			navigator.serviceWorker.controller.postMessage(message);
 			return true;
 		} else {
-			mkSw.showError("Sem controller para enviar mensagem.", navigator.serviceWorker.controller);
+			mksw.showError("Sem controller para enviar mensagem.", navigator.serviceWorker.controller);
 			return false;
 		}
 	}
 	// Update All Clients via Message
 	static requestUpdate = async () => {
-		mkSw.updateConcluido = null;
+		mksw.updateConcluido = null;
 		let c = 0;
-		await mkSw.getUpdate().catch(ree => { re(ree) });
-		if (!mkSw.sendMessageToSW({ action: "UpdateFull" })) {
+		await mksw.getUpdate().catch(ree => { re(ree) });
+		if (!mksw.sendMessageToSW({ action: "UpdateFull" })) {
 			return "Erro de Comunicação";
 		};
 		return new Promise((r, re) => {
 			let esperandoRespostaUpdate = () => {
-				mkSw.showInfo("Registro bem sucedido", c, 4)
-				if (mkSw.updateConcluido != null) {
-					r(mkSw.updateConcluido);
+				mksw.showInfo("Registro bem sucedido", c, 4)
+				if (mksw.updateConcluido != null) {
+					r(mksw.updateConcluido);
 				} else {
 					if (c > 1000) {
 						re("Timeout");
@@ -189,12 +215,6 @@ class mkSw {
 		});
 	}
 
-	// Update All Clients via Message
-	static skipWaiting = async () => {
-		// Try Skip SE houver registro e SE houver algum waiting.
-		(await navigator.serviceWorker?.getRegistration()).waiting?.postMessage({ action: "skipWaiting" });
-	}
-
 	static _getVersaoAtual = () => {
 		if (!localStorage.sw_VersaoAtual) localStorage.sw_VersaoAtual = "0.0";
 		return localStorage.sw_VersaoAtual;
@@ -203,4 +223,108 @@ class mkSw {
 	static _setVersaoAtual = (versao) => {
 		localStorage.sw_VersaoAtual = versao.toString();
 	};
+
+	// Checagem se já está instalado
+	static _isInstalled = async () => {
+		const hasInstalledRelatedApps = (('getInstalledRelatedApps' in navigator) && (await navigator.getInstalledRelatedApps()).length > 0);
+		const standalone = matchMedia('(display-mode: standalone)').matches || matchMedia('(display-mode: tabbed)').matches;
+		console.log(hasInstalledRelatedApps, standalone);
+		return hasInstalledRelatedApps || standalone;
+	}
 }
+
+(async () => {
+	// COLETA
+	// mksw.installContainer = document.querySelector('#installation');
+
+	// if (installContainer) {
+	// 	installContainer.style.display = await mksw._isInstalled() ? 'none' : 'block';
+	// }
+
+	// // const installButton = document.querySelector('#install-button');
+	// const installDialog = document.querySelector('#install-dialog');
+	// const closeButton = document.querySelector('#close-install-dialog');
+	// const backButton = document.querySelector('#back-button');
+	// const forwardButton = document.querySelector('#forward-button');
+	// const screenShots = document.querySelector('#install-dialog .screenshots');
+	// const scrollDiv = screenShots.querySelector('#install-dialog .screenshots .scroll-div');
+	// const innerDiv = scrollDiv.querySelector('div');
+	// let curPos;
+
+	// const supportsInstallPrompt = 'onbeforeinstallprompt' in window;
+
+	// window.addEventListener('load', e => {
+	// 	if (mksw.installButton) {
+	// 		mksw.installButton.disabled = window.deferredPrompt === undefined ? supportsInstallPrompt : false;
+	// 	}
+	// });
+
+	// window.addEventListener('beforeinstallprompt', e => {
+	// 	e.preventDefault();
+
+	// 	if (mksw.installButton) {
+	// 		mksw.installButton.disabled = false;
+	// 	}
+	// });
+
+	// if (mksw.installButton) {
+	// 	if (!supportsInstallPrompt) {
+	// 		const template = getInstallSheetTemplate();
+
+	// 		if (!installDialog.querySelector('.body')) {
+	// 			screenShots.insertAdjacentHTML('beforebegin', template);
+	// 		}
+	// 	}
+
+	// 	mksw.installButton.addEventListener('click', () => {
+	// 		if (window.deferredPrompt) {
+	// 			window.deferredPrompt.prompt();
+	// 		}
+	// 		else if (!supportsInstallPrompt) {
+	// 			curPos = 0;
+	// 			scrollDiv.scrollLeft = 0;
+	// 			installDialog.showModal();
+	// 			installDialog.querySelector('.body').scrollTop = 0;
+
+	// 			setTimeout(() => {
+	// 				installDialog.setAttribute('opened', '');
+	// 			})
+	// 		}
+	// 	});
+	// }
+
+	// installDialog.addEventListener('transitionend', (e) => {
+	// 	if (!installDialog.hasAttribute('opened')) {
+	// 		installDialog.close();
+	// 	}
+	// });
+
+	// closeButton.addEventListener('click', e => {
+	// 	installDialog.removeAttribute('opened');
+
+	// 	// fix for < iOS 17.2, when the install dialog is shown and closed the user can no longer scroll the page
+	// 	// by removing overflow:hidden from the main content and reapplying it with a short delay this is fixed
+	// 	const mainContent = document.querySelector('#main-content');
+	// 	mainContent.style.overflow = 'auto';
+	// 	setTimeout(() => {
+	// 		mainContent.style.overflow = 'hidden';
+	// 	}, 100);
+	// });
+
+	// backButton.addEventListener('click', e => {
+	// 	curPos = Math.max(scrollDiv.scrollLeft - 276, 0);
+	// 	scrollDiv.scrollTo({ left: curPos, behavior: 'smooth' });
+	// });
+
+	// forwardButton.addEventListener('click', e => {
+	// 	curPos = Math.min(scrollDiv.scrollLeft + 276, 552);
+	// 	scrollDiv.scrollTo({ left: curPos, behavior: 'smooth' });
+	// });
+
+	// window.addEventListener('appinstalled', e => {
+	// 	if (mksw.installButton) {
+	// 		mksw.installButton.disabled = true;
+	// 	}
+	// });
+})();
+
